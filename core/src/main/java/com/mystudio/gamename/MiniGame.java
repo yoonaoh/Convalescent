2c8933c8c5a8b890c8e3f402c41783d360e71d97
GaiSawant <grs7@rice.edu>
2020-02-04 16:17:08 -0600

    Updated abstract class for minigame

--- core/src/main/java/com/mystudio/gamename/MiniGame.java
+++ core/src/main/java/com/mystudio/gamename/MiniGame.java
@@ -3,26 +3,57 @@ package com.mystudio.gamename;
 import org.mini2Dx.core.graphics.Graphics;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public interface MiniGame {
+public abstract class MiniGame {
 
-    boolean started = false;
-    boolean ended = false;
+    private boolean open = false;
+    private boolean ended = false;
 
     /**
      * Used as a response to a mouse click.
      */
-    void update(int x, int y); // (x,y) is where cursor click occurs
+    abstract void update(int x, int y); // (x,y) is where cursor click occurs
 
     /**
      * Used for things that need to be updated with time.
      */
-    void interpolate(float alpha);
+    abstract void interpolate(float alpha);
 
     /**
      * Used to actually do the drawing on screen.
      */
-    void render(SpriteBatch batch, Graphics g); // Use either but not both!
+    abstract void render(SpriteBatch batch, Graphics g); // Use either but not both! Preferably use batch.
 
-    void dispose();
+    /**
+     * Used to dispose objects used to reduce time.
+     */
+    abstract void dispose();
+
+    /**
+     * Opens the game in current state
+     */
+    public void open() {
+        open = true;
+    }
+
+    /**
+     * Closes the game if incomplete
+     */
+    public void close() {
+        open = false;
+    }
+
+    /**
+     * Checks if game is open
+     */
+    public boolean isOpen() {
+        return open;
+    }
+
+    /**
+     * Checks if game has ended.
+     */
+    public boolean hasEnded() {
+        return ended;
+    }
 
 }
