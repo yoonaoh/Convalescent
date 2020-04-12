488114c8f7b7bdcd7656a7393071724fd97d5d63
TheraFu <therafuminqi@gmail.com>
2020-04-12 15:32:22 -0500

    merge

--- core/src/main/java/com/mystudio/gamename/views/DarkAttic.java
+++ core/src/main/java/com/mystudio/gamename/views/DarkAttic.java
@@ -1,62 +1,62 @@
-package com.mystudio.gamename.views;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.mystudio.gamename.items.Item;
-import com.mystudio.gamename.items.MinigameTrigger;
-import com.mystudio.gamename.items.SceneTrigger;
-import com.mystudio.gamename.mazepuzzle.MazePuzzleGame;
-import com.mystudio.gamename.utils.GameState;
-import com.mystudio.gamename.utils.MainAdapter;
-import com.mystudio.gamename.windows.MiniGame;
-import org.mini2Dx.core.engine.geom.CollisionBox;
-import org.mini2Dx.core.geom.Polygon;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class DarkAttic extends View {
-    public DarkAttic(final MainAdapter mainAdapter) {
-        super(mainAdapter);
-        background = new Texture("views/attic_bg_dark.png");
-        floorspace = new Polygon(new float[]{
-                80, 0,
-                616, 277,
-                1035, 277,
-                1035, 250,
-                1185, 250,
-                1185, 277,
-                1250, 277,
-                1280, 250,
-                1280, 0
-        });
-        includesAvery = true;
-        includesInventory = false;
-
-        // Add label
-        final Label lightPrompt = new Label("It's dark..", mainAdapter.getManager().getSkin());
-        lightPrompt.setWrap(true);
-        lightPrompt.setWidth(1180);
-        lightPrompt.setPosition(50, 600);
-        actors.addActor(lightPrompt);
-
-        // Add window
-        SceneTrigger window = new SceneTrigger(null, new CollisionBox(840, 380, 150, 160), GameState.ATTIC, mainAdapter);
-        window.setSoundEffect(Gdx.audio.newSound(Gdx.files.internal("sounds/blinds_open.mp3")));
-        actors.addActor(window);
-
-        // Add shelf
-        Item shelf = new Item("views/shelf_dark.png", new CollisionBox(1035, 250, 150, 270));
-        actors.addActor(shelf);
-
-
-        // Add trigger for the maze game
-        MiniGame mazeGame = new MazePuzzleGame(mainAdapter);
-        MinigameTrigger
-                mazeGameTrigger = new MinigameTrigger("mazepuzzle/bug.png", new CollisionBox(0, 0, 100, 100), mazeGame, mainAdapter);
-        actors.addActor(mazeGameTrigger);
-    }
-}
+//package com.mystudio.gamename.views;
+//
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.scenes.scene2d.InputEvent;
+//import com.badlogic.gdx.scenes.scene2d.ui.Label;
+//import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+//import com.mystudio.gamename.items.Item;
+//import com.mystudio.gamename.items.MinigameTrigger;
+//import com.mystudio.gamename.items.SceneTrigger;
+//import com.mystudio.gamename.mazepuzzle.MazePuzzleGame;
+//import com.mystudio.gamename.utils.GameState;
+//import com.mystudio.gamename.utils.MainAdapter;
+//import com.mystudio.gamename.windows.MiniGame;
+//import org.mini2Dx.core.engine.geom.CollisionBox;
+//import org.mini2Dx.core.geom.Polygon;
+//
+//import java.util.Timer;
+//import java.util.TimerTask;
+//
+//public class DarkAttic extends View {
+//    public DarkAttic(final MainAdapter mainAdapter) {
+//        super(mainAdapter);
+//        background = new Texture("views/attic_bg_dark.png");
+//        floorspace = new Polygon(new float[]{
+//                80, 0,
+//                616, 277,
+//                1035, 277,
+//                1035, 250,
+//                1185, 250,
+//                1185, 277,
+//                1250, 277,
+//                1280, 250,
+//                1280, 0
+//        });
+//        includesAvery = true;
+//        includesInventory = false;
+//
+//        // Add label
+//        final Label lightPrompt = new Label("It's dark..", mainAdapter.getManager().getSkin());
+//        lightPrompt.setWrap(true);
+//        lightPrompt.setWidth(1180);
+//        lightPrompt.setPosition(50, 600);
+//        actors.addActor(lightPrompt);
+//
+//        // Add window
+//        SceneTrigger window = new SceneTrigger(null, new CollisionBox(840, 380, 150, 160), GameState.ATTIC, mainAdapter);
+//        window.setSoundEffect(Gdx.audio.newSound(Gdx.files.internal("sounds/blinds_open.mp3")));
+//        actors.addActor(window);
+//
+//        // Add shelf
+//        Item shelf = new Item("views/shelf_dark.png", new CollisionBox(1035, 250, 150, 270));
+//        actors.addActor(shelf);
+//
+//
+//        // Add trigger for the maze game
+//        MiniGame mazeGame = new MazePuzzleGame(mainAdapter);
+//        MinigameTrigger
+//                mazeGameTrigger = new MinigameTrigger("mazepuzzle/bug.png", new CollisionBox(0, 0, 100, 100), mazeGame, mainAdapter);
+//        actors.addActor(mazeGameTrigger);
+//    }
+//}
--- core/src/main/java/com/mystudio/gamename/views/Menu.java
+++ core/src/main/java/com/mystudio/gamename/views/Menu.java
@@ -1,7 +1,6 @@
 package com.mystudio.gamename.views;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
